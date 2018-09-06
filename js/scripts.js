1cdc4d45a777d6d5c5391d8e6c88b6f49fa19c6b
Safiy Cham <csafiy@yahoo.com>
2018-09-05 17:16:23 -0700

    Add projects to display, and work on a whole lot of html/css

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,27 @@
 window.onscroll = function() {fixedToTop()};
 
 let navLinks = document.getElementById("nav_links");
-
 let navLinksSticky = navLinks.offsetTop;
 
 function fixedToTop() {
+
     if (window.pageYOffset > navLinksSticky) {
         navLinks.classList.add("nav_links_sticky");
     } else {
         navLinks.classList.remove("nav_links_sticky");
     }
 }
+
+// rotate about_me img on page load to let user realize page is interactive
+// function getAttention () {
+//     let aboutMe = document.querySelector(".about_me_img");
+
+//     aboutMe.setAttribute("style", "border: 2px solid black");
+    
+//     setTimeout(function () {
+//         aboutMe.removeAttribute("style");
+//     }, 500);
+    
+// }
+
+// getAttention();
