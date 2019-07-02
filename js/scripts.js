b8c6ac6e62562569ddc74ec206ff954c5e25e357
Safiy Cham <csafiy@yahoo.com>
2019-07-02 10:06:47 -0700

    More styling

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,62 @@
 window.onscroll = function () { fixedToTop() };
 
 let navLinks = document.getElementById("nav_links");
-let navLinksSticky = navLinks.offsetTop;
+// let navLinksSticky = navLinks.offsetTop;
 
+// navbar sticks to top on scroll
 function fixedToTop() {
-
-  if (window.pageYOffset > navLinksSticky) {
+  // if (window.pageYOffset > navLinksSticky) {
+  if (window.pageYOffset > 1) {
     navLinks.classList.add("nav_links_sticky");
   } else {
     navLinks.classList.remove("nav_links_sticky");
   }
 }
+
+// multi sliders on a single page
+var sliderObjects = [];
+createSliderObjects();
+
+function slideImage(obj, n) {
+  var parentDiv = $(obj).parent();
+  var matchedDiv;
+  $.each(sliderObjects, function (i, item) {
+    if ($(parentDiv[0]).attr('id') == $(item).attr('id')) {
+      matchedDiv = item;
+      return false;
+    }
+  });
+  matchedDiv.slideIndex = matchedDiv.slideIndex + n;
+  showDivs(matchedDiv, matchedDiv.slideIndex);
+}
+
+function createSliderObjects() {
+  var sliderDivs = $('.slider');
+  $.each(sliderDivs, function (i, item) {
+    var obj = {};
+    obj.id = $(item).attr('id');
+    obj.divContent = item;
+    obj.slideIndex = 1;
+    obj.slideContents = $(item).find('.slide');
+    showDivs(obj, 1);
+    sliderObjects.push(obj);
+  });
+}
+
+function showDivs(divObject, n) {
+  var i;
+  if (n > divObject.slideContents.length) {
+    divObject.slideIndex = 1
+  }
+  if (n < 1) {
+    divObject.slideIndex = divObject.slideContents.length
+  }
+  for (i = 0; i < divObject.slideContents.length; i++) {
+    divObject.slideContents[i].style.display = "none";
+  }
+  divObject.slideContents[divObject.slideIndex - 1].style.display = "block";
+}
+//end multi sliders
 
 // get current year
 let date = new Date();
